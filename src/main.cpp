c25d78de999b1024ebb1141994f2e2303c41aa82
aphd <aphidarium@proton.me>
2025-07-30 01:28:07 +0100

    order of operations

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 #define PRINTC(x) std::cout << #x": " << x << ",\t";
 #define PRINTN(x) std::cout << #x": " << x << "\n";
@@ -92,6 +93,7 @@ std::string int_to_str(int p_int) {
 void tokenise(std::vector<Token>& tokens, std::string_view statement) {
   std::string lexeme{};
   Token::Type prev_type{};
+
   for (int i = 0; i < statement.size(); i++) {
     char ch = statement.at(i);
 
@@ -122,41 +124,58 @@ void tokenise(std::vector<Token>& tokens, std::string_view statement) {
     if (curr_type != Token::Type::Undefined)
       lexeme += ch;
 
-    // PRINTC(lexeme);
-    // PRINTC(ch);
-    // PRINTC(curr_type);
-    // PRINTN(prev_type);
-
     prev_type = curr_type;
   }
 }
 
 void parse(std::vector<Token>& tokens) {
+  std::vector<int> operators{};
   for (int i = 0; i < tokens.size(); i++) {
-    Token& token = tokens[i];
-    if (is_operator(token)) {
+    if (is_operator(tokens[i])) {
       if ((i == 0 || i >= tokens.size()-1) || (is_operator(tokens[i-1]) || is_operator(tokens[i+1]))) {
         error("invalid operator usage");
-        tokens.clear();
-        break;
+        return;
       }
-
-      int lhs = str_to_int(tokens[i-1].lexeme);
-      int rhs = str_to_int(tokens[i+1].lexeme);
-
-      int r{};
-      switch (token.type) {
-      case Token::Type::Divide:   r = lhs / rhs; break;
-      case Token::Type::Multiply: r = lhs * rhs; break;
-      case Token::Type::Plus:     r = lhs + rhs; break;
-      case Token::Type::Minus:    r = lhs - rhs; break;
-      }
-
-      token = {int_to_str(r), Token::Type::Digit};
-      tokens.erase(tokens.begin() + i - 1); // delete lhs
-      tokens.erase(tokens.begin() + i);     // delete rhs
-      i -= 2;
+      operators.push_back(i);
     }
+  }
+
+  std::sort(operators.begin(), operators.end(), [tokens](int a, int b) {
+                                                  return tokens[a].type < tokens[b].type;
+                                                });
+
+  for (int i = 0; i < operators.size(); i++) {
+    int t_i = operators[i];
+    Token& token = tokens[t_i];
+
+    int lhs_i = t_i - 1;
+    int rhs_i = t_i + 1;
+
+    for (; lhs_i > 0, tokens[lhs_i].type != Token::Type::Digit; lhs_i--) {
+      if (tokens[lhs_i].type != Token::Type::Undefined)
+        error("operators require a digit on their left side");
+    }
+
+    for (; rhs_i > 0, tokens[rhs_i].type != Token::Type::Digit; rhs_i++) {
+      if (tokens[rhs_i].type != Token::Type::Undefined)
+        error("operators require a digit on their right side");
+    }
+
+    int lhs = str_to_int(tokens[lhs_i].lexeme);
+    int rhs = str_to_int(tokens[rhs_i].lexeme);
+
+    int r{};
+    switch (token.type) {
+    case Token::Type::Divide:   r = lhs / rhs; break;
+    case Token::Type::Multiply: r = lhs * rhs; break;
+    case Token::Type::Plus:     r = lhs + rhs; break;
+    case Token::Type::Minus:    r = lhs - rhs; break;
+    }
+
+    token = {int_to_str(r), Token::Type::Digit};
+
+    tokens[lhs_i] = {"", Token::Type::Undefined};
+    tokens[rhs_i] = {"", Token::Type::Undefined};
   }
 }
 
@@ -173,7 +192,8 @@ int main() {
     parse(tokens);
 
     for (Token& t : tokens)
-      std::cout << "[" << t.lexeme << ", " << t.type << "]\n";
+      if (t.type != Token::Type::Undefined)
+        std::cout << t.lexeme << "\n";
 
     tokens.clear();
     std::cout << "> ";
